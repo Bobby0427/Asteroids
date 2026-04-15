ed3e26605048bbb7116bb6044ac43892902e34e0
Bobby0427 <kenseb427@gmail.com>
2026-04-15 14:39:49 +0800

    this is the files of the projects

---
+++ Asteroids/main.py
@@ -0,0 +1,25 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+def main():
+    pygame.init()
+    print(f"Starting Asteroidswith pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+        dt = (clock.tick(60))/1000
+        
+if __name__ == "__main__":
+    main()
